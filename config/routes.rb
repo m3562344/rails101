35f2ae3eb75a13d8712f91256b46ba9ca13a2c7c
m3562344 <m3562344@gmail.com>
2017-11-07 15:20:38 +0800

    join & quit group action

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :groups do
+    member do
+      post :quit
+      post :join
+    end
+
+    
     resources :posts
   end
 
-  
+
   root 'groups#index'
 end
